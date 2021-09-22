e9485b142cdd9db4d3932ca23db63eef202b923e
edwarantoni2000 <edwarantoni2000@gmail.com>
2021-09-22 23:40:50 +0700

    update

---
+++ Assets/Scripts/Owl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Owl : Bird
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        if (col.gameObject.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
